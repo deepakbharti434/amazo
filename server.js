e31d1d4879a18cd144cbaf68d0008adc9a52c0be
deepakbharti434 <deepakbharati16@gmail.com>
2021-04-30 17:35:22 +0530

    for deployment

--- server.js
+++ server.js
@@ -4,18 +4,31 @@ import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 import orderRouter from './routers/orderRouter.js';
+import { fileURLToPath } from 'url';
+import path, {dirname} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//app.get('/', express.static(__dirname+'/server/build'))
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-});
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
